8eb1285b16e3d6d91e56e27cffd96cb7ad524c31
dantoledano <155850237+dantoledano@users.noreply.github.com>
2024-01-13 00:46:20 +0200

    Update gameConfig.h

--- gameConfig.h
+++ gameConfig.h
@@ -2,7 +2,7 @@
 class GameConfig
 {
 public:
-	enum class eKeys { LEFT = 'a', RIGHT = 'd', DOWN = 'x',ROTATE='s', CROTATE = 'w', ESC = 27 };
+	enum class eKeys { LEFT = 'a', RIGHT = 'd', DROP = 'x',ROTATE='s', CROTATE = 'w', DOWN='z', ESC = 27 };
 
 	static constexpr int GAME_WIDTH = 12;
 	static constexpr int GAME_HEIGHT = 18;
