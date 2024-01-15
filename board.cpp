279e87c6d24de0b1291e295b3d80d03799ee3a2d
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-15 12:26:14 +0200

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -2,17 +2,13 @@
 
 Board::Board() //ctor
 {
-    for (int i = 0; i < GameConfig::GAME_WIDTH; ++i) {
-        for (int j = 0; j < GameConfig::GAME_HEIGHT; ++j) {
+    for (int i = 0; i < GameConfig::GAME_HEIGHT; ++i) {
+        for (int j = 0; j < GameConfig::GAME_WIDTH; ++j) {
             matrix[i][j] = ' ';
         }
     }
 }
 
 
-char* Board::getMatrix() 
-{
-    return &matrix[0][0];
-}
 
 
