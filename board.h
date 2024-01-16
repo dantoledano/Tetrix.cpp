c8d4b358f372e343eb4c089f8cf02d55419c7976
dantoledano <155850237+dantoledano@users.noreply.github.com>
2024-01-16 21:49:24 +0200

    Update board.h

--- board.h
+++ board.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "gameConfig.h"
 #include "board.h"
+#include "point.h"
 
 class Board
 {
-// private:
-public: 
-	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
-    Board(); //ctor
+	// private:
+public:
+	Point matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
+	Board(); //ctor
+	//void removeFullLines();
+	void eraseLine(int indexLine);
+	bool isFullLine(int line);
+	void clearFullLines();
+	void DrawBoard();
+	void DrawCubeInBoard(int x, int y, char ch);
+	bool isGameOver() const;
 };
 
