9795f0861ced689973c805672e412320f344c5c8
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-19 12:12:16 +0200

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -2,42 +2,73 @@
 #include "point.h"
 #include "general.h"
 #include <Windows.h>
+using namespace std;
 
 
-Board::Board() //ctor
+Board::Board(int xPos): xPos(xPos) //ctor
 {
     for (int i = 0; i < GameConfig::GAME_HEIGHT; ++i) {
         for (int j = 0; j < GameConfig::GAME_WIDTH; ++j) {
-            //matrix[i][j].setActive(false);
-            /*matrix[i][j].setX(j);
-            matrix[i][j].setY(i);*/
-            matrix[i][j]=' ';
+            matrix[i][j] = ' ';
         }
     }
 }
+
+
+//void Board::setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey)
+//{
+//    this->keys[0] = leftKey;
+//    this->keys[1] = rightKey;
+//    this->keys[2] = rotateKey;
+//    this->keys[3] = crotateKey;
+//    this->keys[4] = dropKey;
+//}
+
+
+void Board::drawBorder()
+{
+    for (int col = xPos; col < GameConfig::GAME_WIDTH + xPos; col++)
+    {
+        gotoxy(col, GameConfig::MIN_Y - 1);
+        cout << "-";
+
+        gotoxy(col, GameConfig::GAME_HEIGHT + GameConfig::MIN_Y);
+        cout << "-";
+    }
+
+    for (int row = GameConfig::MIN_Y - 1; row <= GameConfig::GAME_HEIGHT + GameConfig::MIN_Y; row++)
+    {
+        gotoxy(xPos - 1, row);
+        cout << "|";
+
+        gotoxy(GameConfig::GAME_WIDTH + xPos, row);
+        cout << "|";
+    }
+}
+
+
 
 void Board::eraseLine(int indexLine) {
     for (int i = indexLine; i > 0; i--) {
         for (int col = 0; col < GameConfig::GAME_WIDTH; col++) {
-           // matrix[i][col].setActive(matrix[i - 1][col].getActive());
             matrix[i][col] = matrix[i - 1][col];
-            
+
         }
     }
     for (int col = 0; col < GameConfig::GAME_WIDTH; col++) {
-        //matrix[0][col].setActive(false);
         matrix[0][col] = ' ';
     }
 }
 
+
 bool Board::isFullLine(int line) const {
     for (int i = 0; i < GameConfig::GAME_WIDTH; i++) {
-      //  if (!matrix[line][i].getActive()) {
-        if(matrix[line][i]==' ')
+        if (matrix[line][i] == ' ')
             return false;
     }
     return true;
 }
+
 
 void Board::clearFullLines() {
     for (int i = 0; i < GameConfig::GAME_HEIGHT; i++) {
@@ -48,38 +79,31 @@ void Board::clearFullLines() {
     }
 }
 
+
 void Board::DrawCubeInBoard(int x, int y, char ch) {
-    gotoxy(x + GameConfig::MIN_X, y + GameConfig::MIN_Y);
+    gotoxy(x + xPos, y + GameConfig::MIN_Y); ///
     std::cout << ch;
 }
 
-void Board::DrawBoard(){
-    for (int i = GameConfig::GAME_HEIGHT-1; i>=0; i--) {
+
+void Board::DrawBoard() {
+    for (int i = GameConfig::GAME_HEIGHT - 1; i >= 0; i--) {
         for (int j = 0; j < GameConfig::GAME_WIDTH; j++) {
-            if(matrix[i][j]=='#'){
-            //if (matrix[i][j].getActive()) {
-                //matrix[i][j].draw('#');
+            if (matrix[i][j] == '#') {
                 DrawCubeInBoard(j, i, '#');
             }
             else {
-                //matrix[i][j].draw(' ');
                 DrawCubeInBoard(j, i, ' ');
             }
         }
     }
 }
 
-bool Board::isGameOver() const {
-    for (int i = 0; i < GameConfig::GAME_WIDTH; i++) {
-        //if (matrix[1][i].getActive()) {
-            if(matrix[1][i]=='#'){
-            // If any cell in the top row contains a shape, the game is over
-            std::cout << "game over";
-            return true;
-        }
-    }
-    return false;
-}
+
+
+
+
+
 
 
 
