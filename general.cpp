94c026b40498e83a2aaece1ec4b49fad29970396
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-19 12:14:09 +0200

    Update general.cpp

--- general.cpp
+++ general.cpp
@@ -24,29 +24,6 @@ void clrscr()
 }
 
 
-void drawBorder()
-{
-	for (int col = GameConfig::MIN_X; col < GameConfig::GAME_WIDTH + GameConfig::MIN_X; col++)
-	{
-		gotoxy(col, GameConfig::MIN_Y - 1);
-		cout << "-";
-
-		gotoxy(col, GameConfig::GAME_HEIGHT + GameConfig::MIN_Y);
-		cout << "-";
-	}
-
-	for (int row = GameConfig::MIN_Y - 1; row <= GameConfig::GAME_HEIGHT + GameConfig::MIN_Y; row++)
-	{
-		gotoxy(GameConfig::MIN_X - 1, row);
-		cout << "|";
-
-		gotoxy(GameConfig::GAME_WIDTH + GameConfig::MIN_X, row);
-		cout << "|";
-	}
-}
-
-
-
 char randomType() { 
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
 	// Array of characters
