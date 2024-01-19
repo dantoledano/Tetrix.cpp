8aa906ee92416c4a54e90c743019b8845170caf2
dantoledano <155850237+dantoledano@users.noreply.github.com>
2024-01-19 21:52:29 +0200

    Update board.h

--- board.h
+++ board.h
@@ -6,11 +6,12 @@
 class Board
 {
 	int xPos;
-	//char keys[5];
+	//GameConfig::eKeys keys;
 public:
+	char keys[5];
 	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
 	Board(int xPos); //ctor
-	//void setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey);
+	void setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey);
 	int getLeft() { return xPos; }
 	void drawBorder();
 	void eraseLine(int indexLine);
