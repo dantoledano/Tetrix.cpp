e74f082cdcc619ef3641aa7e9eae593c84b91795
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-19 12:14:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,38 +2,15 @@
 using namespace std;
 
 
-void test();
-
 
 int main()
 {
-	test();
+	Game game;
+	game.run();
 	gotoxy(0, GameConfig::GAME_HEIGHT + GameConfig::MIN_Y + 2);
 }
 
 
-void test()
-{
-	Board b1;
-	drawBorder();
-	Shape s;
-	while (true)
-	{
-		s.init(randomType());
-		while (!s.hasReachedBottom(s) && !s.hasReachedToAnotherShape(s,b1))
-		{
-			Sleep(700);
-			int keyPressed = 'z'; // down
-			if (_kbhit())
-			{
-				keyPressed = _getch();
-				if (keyPressed == (int)GameConfig::eKeys::ESC)
-					break;
-			}
-			s.move((GameConfig::eKeys)keyPressed, s, b1);
-		}
 
-	}
-}
 
 
