0105a0e1114cc5ccc61b86c1c0c9d193b9a35fea
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-19 12:15:46 +0200

    Update point.h

--- point.h
+++ point.h
@@ -6,17 +6,13 @@ class Point
 {
 	int x;
 	int y;
-	//bool isActive = false;
 public:
 	void initPoint(int x, int y);
-	int getX() const { return this->x; }; // const beacause its a read-only function
-	int getY() const { return this->y; }; // const beacause its a read-only function
-	//bool getActive() const { return this->isActive; }
-	void setX(const int& _x) { x = _x; }  // const byRef is more efficient
-	void setY(const int& _y) { y = _y; }  // const byRef is more efficient
-	//void setActive(const bool& flag) { isActive = flag; }
-	void draw(char ch);
+	void draw(char ch, int left, int top);
+	int getX() const { return this->x; }; 
+	int getY() const { return this->y; }; 
+	void setX(const int& _x) { x = _x; } 
+	void setY(const int& _y) { y = _y; }  
 	void movePoint(GameConfig::eKeys key);
 
 };
-
