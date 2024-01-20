3744b42980ee95ea5848a7df88e53ad6547778d1
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-20 23:44:30 +0200

    Update gameConfig.h

--- gameConfig.h
+++ gameConfig.h
@@ -2,12 +2,7 @@
 class GameConfig
 {
 public:
-	enum class eKeys { LEFT = 'a', LEFT_CAP = 'A', RIGHT = 'd', RIGHT_CAP = 'D', DROP = 'x', DROP_CAP= 'X',
-		               ROTATE = 's', ROTATE_CAP = 'S', CROTATE = 'w', CROTATE_CAP = 'W', DOWN = 'z', ESC = 27 };
-
-	//enum class eKeysPlayer2 {
-	//	LEFT = 'j', LEFT_CAP = 'J', RIGHT = 'l', RIGHT_CAP = 'L', DROP = 'm', DROP_CAP = 'M',
-	//	ROTATE = 'k', ROTATE_CAP = 'K', CROTATE = 'i', CROTATE_CAP = 'I', DOWN = 'z', ESC = 27};
+	enum class eKeys {LEFT = 'a', RIGHT = 'd', DOWN = 'z', ESC = 27};
 
 	static constexpr int GAME_WIDTH = 12;
 	static constexpr int GAME_HEIGHT = 18;
