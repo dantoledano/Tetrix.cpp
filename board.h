1ff5bf2bb06479b9ddd5a852c17d9f0326265023
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-01-21 22:14:12 +0200

    Update board.h

--- board.h
+++ board.h
@@ -8,11 +8,11 @@ class Board
 	int xPos;
 	int score;
 public:
-	Board(int xPos, int score); //ctor - set xPos of player
-	int getScore() const { return this-> score; }
-	void setScore(const int& score_) { score = score_; }
+	Board(int xPos, int score); //ctor - set xMIN of player, set score
 	char keys[5];
 	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
+	int getScore() const { return this->score; }
+	void setScore(const int& score_) { score = score_; }
 	void setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey);
 	int getLeft() { return xPos; }
 	void drawBorder() const;
