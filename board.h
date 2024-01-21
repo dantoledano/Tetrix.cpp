2f34c35227603f1b1c06816e402ced4a83e6672b
dantoledano <155850237+dantoledano@users.noreply.github.com>
2024-01-21 18:26:07 +0200

    Update board.h

--- board.h
+++ board.h
@@ -7,15 +7,16 @@ class Board
 {
 	int xPos;
 public:
-	Board(int xPos); //ctor - set xPos of player
+	Board(int xPos, int score); //ctor - set xPos of player
+	int score;
 	char keys[5];
 	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
 	void setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey);
 	int getLeft() { return xPos; }
-	void drawBorder();
+	void drawBorder() const;
 	void eraseLine(int indexLine);
 	bool isFullLine(int line) const;
-	void clearFullLines();
+	int clearFullLines();
 	void DrawBoard();
 	void DrawCubeInBoard(int x, int y, char ch);
 	void resetBoard();
