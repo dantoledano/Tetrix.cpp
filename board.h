06ce8e0628455f8ea5120cf795794fdf8ce3d4c1
AliciaBelh <148083807+AliciaBelh@users.noreply.github.com>
2024-02-09 00:08:33 +0200

    Update board.h

--- board.h
+++ board.h
@@ -5,16 +5,20 @@
 
 class Board
 {
-	int xPos;
+	const int xPos;
 	int score;
+	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
+	char keys[GameConfig::NUM_KEYS];
+
 public:
 	Board(int xPos, int score); //ctor - set xMIN of player, set score
-	char keys[5];
-	char matrix[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH];
+	void setMatrixAt(size_t row, size_t col, char value);
+	char getMatrixAt(size_t row, size_t col) const;
 	int getScore() const { return this->score; }
 	void setScore(const int& score_) { score = score_; }
+	char getKeysAt(size_t index) const;
 	void setKeys(char leftKey, char rightKey, char rotateKey, char crotateKey, char dropKey);
-	int getLeft() { return xPos; }
+	int getLeft() const { return this->xPos; }
 	void drawBorder() const;
 	void eraseLine(int indexLine);
 	bool isFullLine(int line) const;
@@ -22,6 +26,6 @@ public:
 	void DrawBoard();
 	void DrawCubeInBoard(int x, int y, char ch);
 	void resetBoard();
-	void expload(int activeX, int activeY);
+	bool expload(int activeX, int activeY);
 	void organizeBoard();
 };
